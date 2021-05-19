93085a2c39f9dd1ac0b2e033d0cc776f227d4c19
Cassie Peets <CHAMBERLAINCASSIE@YAHOO.COM>
2021-05-19 14:37:33 -0700

    bug

---
+++ WebApplication5/WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contat Page - The Tech Academy";
+
+            return View();
+        }
+
+        public ActionResult Instructor(int id)
+        {
+            ViewBag.Id = id;
+
+            Instructor dayTimeInstructor = new Instructor
+            {
+                Id = 1,
+                FirstName = "Erik",
+                LastName = "Gross"
+            };
+
+            return View(dayTimeInstructor);
+        }
+
+        public ActionResult Instructors()
+        {
+            List<Instructor> instructors = new List<Instructor>
+            {
+                new Instructor
+                {
+                    Id = 1,
+                    FirstName = "Tanner",
+                    LastName = "Jones"
+                },
+                new Instructor
+                {
+                    Id = 1,
+                    FirstName = "Melissa",
+                    LastName = "Trescot"
+                },
+                new Instructor
+                {
+                    Id = 3,
+                    FirstName = "Cathy",
+                    LastName = "Smith"
+                }
+            };
+
+            return View(instructors);
+        }
+    }
+}
---
+++ WebApplication5/WebApplication5/Models/Instructor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication5.Models;
+
+namespace WebApplication5.Models
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
